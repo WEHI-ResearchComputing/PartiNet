fcdf34c16338f51d673c4e0d32a476838b619842
Julie Iskander <iskander.j@wehi.edu.au>
2024-06-04 12:54:12 +1000

    convert preprocess into two steps

---
+++ scripts/preprocess_step2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task 2 
+#SBATCH --mem=5G 
+#SBATCH -p regular
+#SBATCH --output=/vast/scratch/users/iskander.j/logs/preprocess_%A_%a.out 
+#SBATCH --job-name=preprocess
+#SBATCH --array=1-35
+
+module load topaz
+
+dataset_name=$(sed -n "$SLURM_ARRAY_TASK_ID"p meta/datasets.txt)
+dataset_path=/vast/scratch/users/iskander.j/PartiNet_data
+
+echo "Generating bounding box...."
+./preprocess.py --dataset ${dataset_name} \
+                --datasets_path ${dataset_path} \
+                --bounding_box
+echo "Done."
+
+
+
