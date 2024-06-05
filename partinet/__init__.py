347932afbd73542cb740257435f04962f01f3bf8
Edward Yang <yang.e@wehi.edu.au>
2024-06-06 09:31:41 +1000

    forward click params to dynamicdet detect

--- partinet/__init__.py
+++ partinet/__init__.py
@@ -1,6 +1,16 @@
 import click
+import sys
 
 VERSION = "0.0.1"
+
+def print_params():
+    """
+    Prints all the parameter-value pairs after cli processing with Click
+    """
+    all_params_str = ", ".join(
+        [f"{k}: {v}" for k, v in click.get_current_context().params.items()]
+    )
+    click.echo(f"Invocation:\n    {all_params_str}")
 
 @click.group()
 def main():
@@ -19,7 +29,33 @@ def train2():
     click.echo("This will perform DynamicDet training step 2.")
 
 @main.command()
-def detect():
+@click.option('--cfg', type=str, help='model.yaml path', required=True)
+@click.option('--weight', type=str, help='model.pt path(s)', required=True)
+@click.option('--source', type=str, default='inference/images', help='source', show_default=True)  # file/folder, 0 for webcam
+@click.option('--num-classes', type=int, default=80, help='number of classes', show_default=True)
+@click.option('--img-size', type=int, default=640, help='inference size (pixels)', show_default=True)
+@click.option('--conf-thres', type=float, default=0.25, help='object confidence threshold', show_default=True)
+@click.option('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS', show_default=True)
+@click.option('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu', show_default=True)
+@click.option('--view-img', is_flag=True, help='display results')
+@click.option('--save-txt', is_flag=True, help='save results to *.txt')
+@click.option('--save-conf', is_flag=True, help='save confidences in --save-txt labels')
+@click.option('--nosave', is_flag=True, help='do not save images/videos')
+@click.option('--classes', multiple=True, type=int, default=[], help='filter by class: --classes 0, or --classes 0 --classes 2 --classes 3', show_default=True)
+@click.option('--agnostic-nms', is_flag=True, help='class-agnostic NMS')
+@click.option('--augment', is_flag=True, help='augmented inference')
+@click.option('--project', default='runs/detect', help='save results to project/name', show_default=True)
+@click.option('--name', default='exp', help='save results to project/name', show_default=True)
+@click.option('--exist-ok', is_flag=True, help='existing project/name ok, do not increment')
+@click.option('--dy-thres', type=float, default=0.5, help='dynamic thres', show_default=True)
+def detect(cfg, weight, num_classes, source, img_size, conf_thres, iou_thres, device, view_img, save_txt, save_conf, nosave, classes, agnostic_nms, augment, project, name, exist_ok, dy_thres):
+
+    print_params()
+
     import partinet.DynamicDet.detect
     click.echo("This will perform DynamicDet detection.")
-    partinet.DynamicDet.detect.detect()
+    with partinet.DynamicDet.detect.torch.no_grad():
+        partinet.DynamicDet.detect.detect(cfg, weight, num_classes, source, img_size, conf_thres, iou_thres, device, view_img, save_txt, save_conf, nosave, classes, agnostic_nms, augment, project, name, exist_ok, dy_thres)
+
+if __name__ == "__main__":
+    main()
