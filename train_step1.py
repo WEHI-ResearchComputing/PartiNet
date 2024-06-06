c4f9b1e65e5c981360e2e06815ef8296207176b6
Edward Yang <yang.e@wehi.edu.au>
2024-06-06 11:42:09 +1000

    fix typos

--- train_step1.py
+++ train_step1.py
@@ -23,17 +23,17 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 from torch.utils.tensorboard import SummaryWriter
 
 import test  # import test.py to get mAP after each epoch
-from models.yolo import Model
-from utils.autoanchor import check_anchors
-from utils.datasets import create_dataloader
-from utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
+from partinet.DynamicDet.models.yolo import Model
+from partinet.DynamicDet.utils.autoanchor import check_anchors
+from partinet.DynamicDet.utils.datasets import create_dataloader
+from partinet.DynamicDet.utils.general import labels_to_class_weights, increment_path, labels_to_image_weights, init_seeds, \
     fitness, strip_optimizer, get_latest_run, check_dataset, check_file, check_git_status, check_img_size, \
     set_logging, one_cycle, colorstr
-from utils.loss import ComputeLoss, ComputeLossOTA, ComputeLossOTADual
-from utils.plots import plot_images, plot_results, plot_lr_scheduler
-from utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first, is_parallel
-from utils.wandb_logging.wandb_utils import WandbLogger, check_wandb_resume
-from utils.checkpoint import get_state_dict
+from partinet.DynamicDet.utils.loss import ComputeLoss, ComputeLossOTA, ComputeLossOTADual
+from partinet.DynamicDet.utils.plots import plot_images, plot_results, plot_lr_scheduler
+from partinet.DynamicDet.utils.torch_utils import ModelEMA, select_device, intersect_dicts, torch_distributed_zero_first, is_parallel
+from partinet.DynamicDet.utils.wandb_logging.wandb_utils import WandbLogger, check_wandb_resume
+from partinet.DynamicDet.utils.checkpoint import get_state_dict
 
 
 logger = logging.getLogger(__name__)
@@ -509,63 +509,22 @@ def train(hyp, opt, device, tb_writer=None):
     torch.cuda.empty_cache()
     return results
 
-# def main(cfg, weight, data, hyp, epocs, batch_size, img_size, rect, resume, nosave, notest, noautoanchor, bucket, cache_images, image_weights, device, multi_scale, single_cls, single_backbone, adam, sync_bn, local_rank, workers, project, entity, name, exist_ok, quad, linear_lr, label_smoothing, upload_dataset, bbox_interval, save_period, artifact_alias, freeze, v5_metric):
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
-    parser.add_argument('--weight', type=str, default='', help='initial weights path')
-    parser.add_argument('--data', type=str, default='data/coco.yaml', help='data.yaml path')
-    parser.add_argument('--hyp', type=str, default='hyp/hyp.scratch.p5.yaml', help='hyperparameters path')
-    parser.add_argument('--epochs', type=int, default=300)
-    parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs')
-    parser.add_argument('--img-size', nargs='+', type=int, default=[640, 640], help='[train, test] image sizes')
-    parser.add_argument('--rect', action='store_true', help='rectangular training')
-    parser.add_argument('--resume', nargs='?', const=True, default=False, help='resume most recent training')
-    parser.add_argument('--nosave', action='store_true', help='only save final checkpoint')
-    parser.add_argument('--notest', action='store_true', help='only test final epoch')
-    parser.add_argument('--noautoanchor', action='store_true', help='disable autoanchor check')
-    parser.add_argument('--bucket', type=str, default='', help='gsutil bucket')
-    parser.add_argument('--cache-images', action='store_true', help='cache images for faster training')
-    parser.add_argument('--image-weights', action='store_true', help='use weighted image selection for training')
-    parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--multi-scale', action='store_true', help='vary img-size +/- 50%%')
-    parser.add_argument('--single-cls', action='store_true', help='train multi-class data as single-class')
-    parser.add_argument('--single-backbone', action='store_true', help='train single backbone model')
-    parser.add_argument('--adam', action='store_true', help='use torch.optim.Adam() optimizer')
-    parser.add_argument('--sync-bn', action='store_true', help='use SyncBatchNorm, only available in DDP mode')
-    parser.add_argument('--local_rank', type=int, default=-1, help='DDP parameter, do not modify')
-    parser.add_argument('--workers', type=int, default=8, help='maximum number of dataloader workers')
-    parser.add_argument('--project', default='runs/train', help='save to project/name')
-    parser.add_argument('--entity', default=None, help='W&B entity')
-    parser.add_argument('--name', default='exp', help='save to project/name')
-    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
-    parser.add_argument('--quad', action='store_true', help='quad dataloader')
-    parser.add_argument('--linear-lr', action='store_true', help='linear LR')
-    parser.add_argument('--label-smoothing', type=float, default=0.0, help='Label smoothing epsilon')
-    parser.add_argument('--upload_dataset', action='store_true', help='Upload dataset as W&B artifact table')
-    parser.add_argument('--bbox_interval', type=int, default=-1, help='Set bounding-box image logging interval for W&B')
-    parser.add_argument('--save_period', type=int, default=-1, help='Log model after every "save_period" epoch')
-    parser.add_argument('--artifact_alias', type=str, default="latest", help='version of dataset artifact to be used')
-    parser.add_argument('--freeze', nargs='+', type=int, default=[0], help='Freeze layers: backbone of yolov7=50, first3=0 1 2')
-    parser.add_argument('--v5-metric', action='store_true', help='assume maximum recall as 1.0 in AP calculation')
-    opt = parser.parse_args()
+def main(opt):
 
     # Set DDP variables from environment variables set by torch.distributed.launch or torchrun
-    opt.local_rank = int(os.environ['LOCAL_RANK']) if 'LOCAL_RANK' in os.enriron else -1
+    opt.local_rank = int(os.environ['LOCAL_RANK']) if 'LOCAL_RANK' in os.environ else -1
     opt.world_size = int(os.environ['WORLD_SIZE']) if 'WORLD_SIZE' in os.environ else 1
     opt.global_rank = int(os.environ['RANK']) if 'RANK' in os.environ else -1
 
     # use OMPI variables if being run with OpenMPI mpi_run
-    opt.local_rank = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK']) if 'OMPI_COMM_WORLD_LOCAL_RANK' in os.enriron else -1
+    opt.local_rank = int(os.environ['OMPI_COMM_WORLD_LOCAL_RANK']) if 'OMPI_COMM_WORLD_LOCAL_RANK' in os.environ else -1
     opt.world_size = int(os.environ['OMPI_COMM_WORLD_SIZE']) if 'OMPI_COMM_WORLD_SIZE' in os.environ else 1
     opt.global_rank = int(os.environ['OMPI_COMM_WORLD_RANK']) if 'OMPI_COMM_WORLD_RANK' in os.environ else -1
 
     set_logging(opt.global_rank)
 
     # dump stdout to void if not rank 0
-    if world_rank > 0:
+    if opt.global_rank > 0:
         sys.stdout = open(os.devnull, "w")
     
     opt.total_batch_size = opt.batch_size
@@ -574,7 +533,7 @@ if __name__ == '__main__':
         assert torch.cuda.device_count() > opt.local_rank
         torch.cuda.set_device(opt.local_rank)
         device = torch.device('cuda', opt.local_rank)
-        dist.init_process_group(backend='nccl', init_method='env://',rank=world_rank, world_size=world_size)#, timeout=datetime.timedelta(seconds=200))  # distributed backend
+        dist.init_process_group(backend='nccl', init_method='env://',rank=opt.global_rank, world_size=opt.world_size)#, timeout=datetime.timedelta(seconds=200))  # distributed backend
         assert opt.batch_size % opt.world_size == 0, '--batch-size must be multiple of CUDA device count'
         opt.batch_size = opt.total_batch_size // opt.world_size
 
